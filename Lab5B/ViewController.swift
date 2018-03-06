7d5b4d6f38412882b93ec06f0447712a989d2bc5
Michael Horie <michael@win-dipkvbgoo1a.camosun.ca>
2018-03-06 15:28:24 -0800

    Initial Commit

---
+++ Lab5B/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab5B
+//
+//  Created by Michael Horie on 2018-03-06.
+//  Copyright © 2018 Michael Horie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lab5BTests/Lab5BTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lab5BTests.swift
+//  Lab5BTests
+//
+//  Created by Michael Horie on 2018-03-06.
+//  Copyright © 2018 Michael Horie. All rights reserved.
+//
+
+import XCTest
+@testable import Lab5B
+
+class Lab5BTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
